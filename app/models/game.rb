3300c22fd102afed04560f55a2709c84eb1c47c9
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-10-14 12:25:32 +0200

    Fix validations in wrong class

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ActiveRecord::Base
 	belongs_to :genre
-
+	
 	validates :name, presence: true, length: {maximum: 32}
+	validates :genre_id, presence: true
 	validates :description, length: {maximum: 200}
+	validates :release, length: {minimum: 1900, maximum: 2100}
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,5 @@ class Genre < ActiveRecord::Base
 	has_many :games
 
 	validates :name, presence: true, length: {maximum: 32}
-	validates :genre_id, presence: true
 	validates :description, length: {maximum: 200}
-	validates :release, length: {minimum: 1900, maximum: 2100}
 end
